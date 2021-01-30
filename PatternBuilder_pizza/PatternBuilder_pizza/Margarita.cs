8fdea7aeebd290c0a1bc7084e7d8d4944e2a29b6
AntonZyuzin <71172648+AntonZyuzin@users.noreply.github.com>
2021-01-30 23:15:38 +0300

    Update Margarita.cs

--- PatternBuilder_pizza/PatternBuilder_pizza/Margarita.cs
+++ PatternBuilder_pizza/PatternBuilder_pizza/Margarita.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PatternBuilderPizza
 {
-    class Margarita : Pizza
+    class Margarita : PizzaInterface
     {
         public void SetDough(string dough)
         {
